7cabe7d24d62d7e1391379fca2d15fa6553b806f
IGH26 <planning71.mk@gmail.com>
2026-03-04 12:07:49 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="تطبيق الأخبار IGH", layout="centered")
+
+st.title("📱 تطبيق الأخبار الذكي - IGH")
+st.subheader("مرحباً بكِ! هذا الموقع يعمل بلغة Python")
+
+st.info("نحن الآن نقوم بربط هذا الموقع بقاعدة بياناتك في Supabase.")
